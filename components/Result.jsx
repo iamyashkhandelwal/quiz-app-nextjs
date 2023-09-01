a889030456324fba41d0bcc5ebafc44f0682c521
Yash Khandelwal <kyash3079@gmail.com>
2023-09-01 10:04:47 +0530

    Added table view for results component

--- components/Result.jsx
+++ components/Result.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import styles from './Result.module.css'
 
-const Result = ({ selectedAnswers }) => {
+const Result = ({ selectedAnswers, quizData }) => {
   return (
-    <div>Result - Output</div>
+    <table className={styles.table}>
+      <caption>Quiz Result</caption>
+      <thead>
+          <th>Question</th>
+          <th>Selected Answer</th>
+          <th>Correct Answer</th>
+      </thead>
+      <tbody>
+        {selectedAnswers.map((ans, idx) => (
+          <tr className={`${ans.toLowerCase() === quizData[idx].correctAns.toLowerCase() ? styles.green : styles.red}`}>
+            <th>{idx+1}</th>
+            <td dangerouslySetInnerHTML={{ __html: `${ans || '-'}` }}></td>
+            <td dangerouslySetInnerHTML={{ __html: `${quizData[idx].correctAns}` }}></td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   )
 }
 
-export default Result
+export default Result;
